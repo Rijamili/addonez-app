9d5fea81c262d846a815abbf69b2916621713ea8
Rinsiya <rinsiyashereen447@gmail.com>
2026-07-07 17:42:25 +0530

    updation

--- src/routes/profileRoutes.js
+++ src/routes/profileRoutes.js
@@ -3,4 +3,11 @@ const router  = express.Router();
 const { authenticate } = require("../middleware/auth");
 const { getProfile } = require("../controllers/profileController");
 router.get("/", authenticate, getProfile);
+router.put("/notifications", auth, profileController.updateNotifications);
+router.put(
+  "/change-password",
+  authenticate,
+  profileController.changePassword
+);
+
 module.exports = router;
