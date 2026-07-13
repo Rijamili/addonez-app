93cf2c0d76def95d540046ead7ba6a644abe5628
dataanalystuserone-create <dataanalystuserone@gmail.com>
2026-07-13 21:44:29 +0530

    up

--- src/middleware/validate.js
+++ src/middleware/validate.js
@@ -1,9 +1,26 @@
-// src/middleware/validate.js
+// // src/middleware/validate.js
+// const { validationResult } = require("express-validator");
+// const { error } = require("../utils/response");
+// const validate  = (req, res, next) => {
+//   const errs = validationResult(req);
+//   if (!errs.isEmpty()) return error(res, "Validation failed", 422, errs.array());
+//   next();
+// };
+// module.exports = { validate };
 const { validationResult } = require("express-validator");
 const { error } = require("../utils/response");
-const validate  = (req, res, next) => {
+
+const validate = (req, res, next) => {
   const errs = validationResult(req);
-  if (!errs.isEmpty()) return error(res, "Validation failed", 422, errs.array());
+
+  if (!errs.isEmpty()) {
+    console.log("Validation Errors:", errs.array());
+    console.log("Request Body:", req.body);
+
+    return error(res, "Validation failed", 422, errs.array());
+  }
+
   next();
 };
+
 module.exports = { validate };
