66faec6ff4083f7938fc0af37773686d23ce6c1d
dataanalystuserone-create <dataanalystuserone@gmail.com>
2026-07-22 11:31:43 +0530

    updated

--- src/config/bootstrap.js
+++ src/config/bootstrap.js
@@ -16,7 +16,7 @@ const bootstrap = {
   },
   jwt: {
     secret:    process.env.JWT_SECRET,
-    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+    expiresIn: process.env.JWT_EXPIRES_IN || "90d",
   },
   server: {
     port:            parseInt(process.env.PORT || "5000"),
